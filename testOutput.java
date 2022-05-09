e5df6569386ad6c94f6943f3c4ba5b176976ce7f
glacierEvan <93851399+Evan-Sukhoi@users.noreply.github.com>
2022-05-09 19:15:47 +0800

    Update testOutput.java

--- testOutput.java
+++ testOutput.java
@@ -8,7 +8,7 @@ public class testOutput {
             Properties defprop = new Properties();
             defprop.put("host", "localhost");
             defprop.put("user", "postgres");
-            defprop.put("password", "123456");
+            defprop.put("password", "Lyw1107Ypa");
             defprop.put("database", "proj_2");
             Properties prop = new Properties(defprop);
             dm.openDB(prop.getProperty("host"), prop.getProperty("database"),
@@ -19,8 +19,11 @@ public class testOutput {
             System.out.println(dm.getOrderCount());  //Q8
             System.out.println(dm.getNeverSoldProductCount());  //Q9
             System.out.println(dm.getFavoriteProductModel());  //Q10
+            System.out.println(dm.getAvgStockByCenter());  //Q11
+            System.out.println(dm.getProductByNumber("A50L172"));  //Q12
+            System.out.println(dm.getContractInfo("CSE0000209"));  //Q13
 
-
+            dm.closeDB();
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
